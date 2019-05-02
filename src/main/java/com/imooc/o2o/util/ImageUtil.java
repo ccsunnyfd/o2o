8edd0a9ec3b48a924c7572d66e5f3cbd31d2c0b0
ccsunnyfd <chzhyfd@126.com>
2019-05-02 17:19:53 +0800

    create thumb image process util function(ImageUtil\PathUtil)

---
+++ src/main/java/com/imooc/o2o/util/ImageUtil.java
@@ -0,0 +1,53 @@
+package com.imooc.o2o.util;
+
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Positions;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class ImageUtil {
+    private static String basePath = Thread.currentThread().getContextClassLoader()
+            .getResource("").getPath();
+    private static final SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+    private static final Random r = new Random();
+
+    public void generateThumbnail(CommonsMultipartFile thumbnail, String targetAddr) {
+        String realFileName = getRandomFileName();
+        String extension = getFileExtension(thumbnail);
+        makeDirPath(targetAddr);
+        String relativeAddr = targetAddr + realFileName + extension;
+        File dest = new File(PathUtil.getImgBasePath() + relativeAddr);
+        try {
+            Thumbnails.of(thumbnail.getInputStream()).size(200, 200)
+                    .watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File(basePath + "/watermark.jpg")), 0.25f)
+                    .outputQuality(0.8f).toFile(dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void makeDirPath(String targetAddr) {
+        String realFileParentPath = PathUtil.getImgBasePath() + targetAddr;
+        File dirPath = new File(realFileParentPath);
+        if (!dirPath.exists()) {
+            dirPath.mkdirs();
+        }
+    }
+
+    private static String getFileExtension(CommonsMultipartFile cFile) {
+        String originalFileName = cFile.getOriginalFilename();
+        return originalFileName.substring(originalFileName.lastIndexOf("."));
+    }
+
+    private static String getRandomFileName() {
+        int rannum = r.nextInt(89999) + 10000;
+        String nowTimeStr = sDateFormat.format(new Date());
+        return nowTimeStr + rannum;
+    }
+}
